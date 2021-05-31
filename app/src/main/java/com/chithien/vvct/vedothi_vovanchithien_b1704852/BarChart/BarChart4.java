aed46111ea2dbcb5aca1dfff71b6ebf43c730033
RakiticVo <thienb1704852@gmail.com>
2021-05-31 19:08:23 +0700

    Initial commit

---
+++ app/src/main/java/com/chithien/vvct/vedothi_vovanchithien_b1704852/BarChart/BarChart4.java
@@ -0,0 +1,16 @@
+package com.chithien.vvct.vedothi_vovanchithien_b1704852.BarChart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chithien.vvct.vedothi_vovanchithien_b1704852.R;
+
+public class BarChart4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_chart4);
+    }
+}
---
+++ app/src/main/java/com/chithien/vvct/vedothi_vovanchithien_b1704852/PieChart/pieChart3.java
@@ -0,0 +1,16 @@
+package com.chithien.vvct.vedothi_vovanchithien_b1704852.PieChart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chithien.vvct.vedothi_vovanchithien_b1704852.R;
+
+public class pieChart3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pie_chart3);
+    }
+}
